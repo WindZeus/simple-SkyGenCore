931b8f35f111e7bf24164edc70b72914f1563c97
WindZeus <nhpwind@gmail.com>
2024-12-21 12:25:25 +0700

    hologramListener.java

--- src/listeners/hologramListener.java
+++ src/listeners/hologramListener.java
@@ -5,7 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
 
-import grassminevn.skygencore.Main;
+import supermine.skygencore.Main;
 import manager.GENERATORdatabaseManager;
 
 public class hologramListener implements Listener {
